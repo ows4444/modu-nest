f4ce297f224bf456d949caf93e9f876c2442ff86
Muhammad Awais <ows4444@gmail.com>
2025-08-09 23:42:48 +0500

    fix: update checksum generation to use Uint8Array for buffer

--- libs/plugin-types/src/lib/plugin-utilities.ts
+++ libs/plugin-types/src/lib/plugin-utilities.ts
@@ -11,7 +11,7 @@ export class PluginUtils {
    * Generate a checksum for plugin content
    */
   static generateChecksum(buffer: Buffer): string {
-    return crypto.createHash('sha256').update(buffer).digest('hex');
+    return crypto.createHash('sha256').update(new Uint8Array(buffer)).digest('hex');
   }
 
   /**
